c8087407b46208ec4399a40e24317dd05f59658e
Bernardo Caiado <becaiado@gmail.com>
2020-08-20 12:19:31 -0700

    done basic functionalities

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,31 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new # needed to instantiate the form_for
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+    # no need for app/views/cocktails/create.html.erb
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   # get 'cocktails', to: 'cocktails#index'
+
+   # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+
+   # get 'cocktails/show/:id', to: 'cocktails#new'
+   # post 'cocktails', to: 'cocktails#create'
+
+   # resources :doses
+
+  root to: 'cocktails#index'
+
+  resources :doses, only: :destroy
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
 end
